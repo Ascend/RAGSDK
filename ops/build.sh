d6fe88b4f0c497316fd994a0001a5e92df6b5e4c
developer <developer@com>
2024-06-19 14:07:28 +0800

    Match-id-c926d06459450563bc9509a13449a667e6a11cc3

---
+++ ops/build.sh
@@ -0,0 +1,18 @@
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+$ASCEND_HOME_PATH/python/site-packages/bin/msopgen gen -i bert_self_attention.json -f pytorch -c ai_core-Ascend910B4 -lan cpp -out BertSelfAttention_
+cp -r ./BertSelfAttention/* ./BertSelfAttention_
+rm -rf ./BertSelfAttention/*
+mv ./BertSelfAttention_/* ./BertSelfAttention
+rm -rf BertSelfAttention_
+cd ./BertSelfAttention
+if [ ! -f "CMakePresets.json" ]; then
+    echo "Error: CMakePresets.json File does not exist."
+    exit 1
+fi
+sed -i 's|"/usr/local/Ascend/latest"|"/usr/local/Ascend/ascend-toolkit/latest"|' "CMakePresets.json"
+sed -i 's|"customize"|"mxRAG"|' "CMakePresets.json"
+chmod -R +x ./build.sh
+find . -type f -name "*.sh" -print0 | xargs -0 dos2unix
+bash build.sh
+./build_out/custom_opp_*.run
+cd ..
