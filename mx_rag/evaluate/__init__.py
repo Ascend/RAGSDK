2491043b1675126e4eba1b6d51803e07bf6cf716
developer <developer@com>
2024-08-28 17:09:38 +0800

    Match-id-1df77321c2cca68174ac873958f0c65669c2075e

--- mx_rag/evaluate/__init__.py
+++ mx_rag/evaluate/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
 __all__ = [
-    "model"
+    "Evaluate"
 ]
 
-from mx_rag.evaluate import model
+from mx_rag.evaluate.evaluate import Evaluate
--- mx_rag/evaluate/model/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
-__all__ = [
-    "APILLM",
-    "APIEmbedding",
-    "LocalEmbedding"
-]
-
-from mx_rag.evaluate.model.api_model import APILLM, APIEmbedding
-from mx_rag.evaluate.model.local_model import LocalEmbedding
