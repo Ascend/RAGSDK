48622ceaa8222d5c04edc209c01b7f9a43fd1ac3
developer <developer@com>
2024-09-19 17:06:24 +0800

    Match-id-16bc2f5cdcd927208984d6ce83c67910b01657fc

--- tests/python/reranker/service/test_tei_reranker.py
+++ tests/python/reranker/service/test_tei_reranker.py
@@ -26,7 +26,7 @@ class TestTEIReranker(unittest.TestCase):
             return TestTEIReranker.Result(True, json.dumps(response_data))
 
         with patch('mx_rag.utils.url.RequestUtils.post', mock.Mock(side_effect=mock_post)):
-            rerank = TEIReranker(url='https://localhost:8888')
+            rerank = TEIReranker(url='https://localhost:8888', api_key="xxxx")
 
             texts = ['我是小黑', '我是小红'] * 100
             scores = rerank.rerank(query='你好', texts=texts)
@@ -37,14 +37,14 @@ class TestTEIReranker(unittest.TestCase):
             self.assertEqual(scores.shape, (len(texts),))
 
     def test_empty_texts(self):
-        rerank = TEIReranker(url='https://localhost:8888')
+        rerank = TEIReranker(url='https://localhost:8888', api_key="xxxx")
 
         texts = ["text"]
         scores = rerank.rerank(query='你好', texts=texts)
         self.assertEqual(scores.shape, (0,))
 
     def test_texts_too_long(self):
-        rerank = TEIReranker(url='https://localhost:8888')
+        rerank = TEIReranker(url='https://localhost:8888', api_key="xxxx")
 
         texts = ['我是小黑', '我是小红'] * 500001
         with self.assertRaises(ValueError):
@@ -55,7 +55,7 @@ class TestTEIReranker(unittest.TestCase):
             return TestTEIReranker.Result(False, "")
 
         with patch('mx_rag.utils.url.RequestUtils.post', mock.Mock(side_effect=mock_post)):
-            rerank = TEIReranker(url='https://localhost:8888')
+            rerank = TEIReranker(url='https://localhost:8888', api_key="xxxx")
 
             texts = ['我是小黑', '我是小红'] * 300
             scores = rerank.rerank(query='你好', texts=texts)
