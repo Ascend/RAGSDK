24ed24043fa067b317ef5d3018d53f365d718ba6
developer <developer@com>
2024-09-19 16:42:54 +0800

    Match-id-80816296b10f81ed06bbceb6e76e6503901292d8

--- build/package.sh
+++ build/package.sh
@@ -6,13 +6,9 @@ set -e
 
 warn() { echo >&2 -e "\033[1;31m[WARN ][Depend  ] $1\033[1;37m" ; }
 ARCH=$(uname -m)
-PY_VER=py$(python3.10 -c "import platform; print(platform.python_version().replace('.','')[0:1])")
 CUR_PATH=$(dirname "$(readlink -f "$0")")
 ROOT_PATH=$(readlink -f "${CUR_PATH}"/..)
 PKG_DIR=mindxsdk-mxrag
-CI_PACKAGE_DIR="${ROOT_PATH}"/output/"${PKG_DIR}"
-OUTPUT_DIR="${ROOT_PATH}"/_package_output_${PY_VER}
-
 
 VERSION_FILE="${ROOT_PATH}"/../mindxsdk/build/conf/config.yaml
 get_version() {
@@ -28,38 +24,29 @@ get_version() {
 
 get_version
 echo "MindX SDK mxrag: ${VERSION}" >> "$ROOT_PATH"/version.info
-RELEASE_TAR=Ascend-"${PKG_DIR}"_"${VERSION}"_linux-"${ARCH}".tar.gz
 
 
 function package()
 {
-    # copy package
-    mkdir -p "${OUTPUT_DIR}"
-    cp -r "${ROOT_PATH}"/dist/* "${OUTPUT_DIR}"/
-    echo "copy mx_rag package to output dir."
+    py_version=$1
 
-    # clean
-    if [ -d "${CI_PACKAGE_DIR}" ]; then
-        rm -rf "${CI_PACKAGE_DIR}"
+    if [ -z "$py_version" ]; then
+        echo "python version invalid"
+        exit 1
     fi
-    echo "clean ci package output dir"
+
+    ci_package_dir="${ROOT_PATH}"/output/"${PKG_DIR}"/${py_version}/${PKG_DIR}
+    # clean
+    [ -n "$ci_package_dir" ] && rm -rf "${ci_package_dir}"
 
     # package
-    mkdir -p -m 700 "${CI_PACKAGE_DIR}"
-    echo "start build output package"
-    cd "${OUTPUT_DIR}"
-    cp *.whl "${CI_PACKAGE_DIR}"
+    mkdir -p -m 700 "${ci_package_dir}"
+    cp -rf "${ROOT_PATH}"/dist/mx_rag*"${py_version}"*.whl "${ci_package_dir}"
 
-    if [[ "$OPP_RUN_NAME" == *"aarch64"* ]]; then
-      OPP_RUN_NAME=$( echo "${OPP_RUN_NAME}" | awk -F'_' '{print $1 "_" $2 "_" $4 }')
-    else
-      OPP_RUN_NAME=$( echo "${OPP_RUN_NAME}" | awk -F'_' '{print $1 "_" $2 "_" $4 ".run"}')
-    fi
+    mv "${ROOT_PATH}"/version.info "${ci_package_dir}"
+    cp -rf "${ROOT_PATH}"/requirements.txt "${ci_package_dir}"
 
-    mv "${ROOT_PATH}"/version.info "${CI_PACKAGE_DIR}"
-    cp "${ROOT_PATH}"/requirements.txt "${CI_PACKAGE_DIR}"
-
-    cd "${CI_PACKAGE_DIR}"
+    cd "${ci_package_dir}"
     #将所有目录设置为550，特殊目录单独处理
     find ./ -type d -exec chmod 550 {} \;
     #将所有文件设置为440，特殊文件单独处理
@@ -68,14 +55,12 @@ function package()
     find ./  \( -name "*.sh" -o -name "*.run" \)  -exec  chmod 550 {} \;
 
     cd ../
-    tar -zcvf "${RELEASE_TAR}" "${PKG_DIR}" || {
-      warn "compression failed, packages might be broken"
-    }
+    tar -zcvf "${ROOT_PATH}"/output/Ascend-"${PKG_DIR}"_"${VERSION}"_"${py_version}"_linux-"${ARCH}".tar.gz "${PKG_DIR}"
 }
 
 function main()
 {
-    package
+    package "$1"
 }
 
-main
+main "$@"
