1b385948c4087dcf68e033661377c1db541384c5
developer <developer@com>
2024-10-15 20:05:56 +0800

    Match-id-99a3c746d78c8a4009edbfab6297849d64b6ecbb

--- mx_rag/document/loader/ppt_loader.py
+++ mx_rag/document/loader/ppt_loader.py
@@ -15,6 +15,8 @@ from mx_rag.utils.file_check import SecFileCheck, FileCheckError, PathNotFileExc
 class PowerPointLoader(BaseLoader, mxBaseLoader):
     EXTENSION = (".pptx",)
     MAX_SIZE = 100 * 1024 * 1024
+    MAX_TABLE_ROW = 100
+    MAX_TABLE_COL = 50
 
     def __init__(self, file_path, lang="ch"):
         super().__init__(file_path)
@@ -49,8 +51,8 @@ class PowerPointLoader(BaseLoader, mxBaseLoader):
                 data[span_row][span_col] = cell.text
 
     def _load_table(self, table):
-        rows = len(table.rows)
-        cols = len(table.columns)
+        rows = min(len(table.rows), self.MAX_TABLE_ROW)
+        cols = min(len(table.columns), self.MAX_TABLE_COL)
         # 初始化一个二维列表来存储表格数据
         data = [["" for _ in range(cols)] for _ in range(rows)]
         for row in range(rows):
