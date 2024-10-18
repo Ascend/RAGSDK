9fa5c3855db98b43eb739301d7a7372fac6c3a3b
developer <developer@com>
2024-10-18 17:42:31 +0800

    Match-id-6195e6cfa51dfb77a7961e70b79918a319e21916

--- mx_rag/knowledge/handler.py
+++ mx_rag/knowledge/handler.py
@@ -10,8 +10,8 @@ from mx_rag.knowledge.base_knowledge import KnowledgeBase
 from mx_rag.knowledge.doc_loader_mng import LoaderMng
 from mx_rag.knowledge.knowledge import KnowledgeDB
 from mx_rag.utils.common import validate_params, BOOL_TYPE_CHECK_TIP, CALLABLE_TYPE_CHECK_TIP, NO_SPLIT_FILE_TYPE, \
-    STR_LENGTH_CHECK_1024
-from mx_rag.utils.file_check import SecFileCheck
+    STR_LENGTH_CHECK_1024, FILE_COUNT_MAX
+from mx_rag.utils.file_check import SecFileCheck, FileCheck
 
 
 class FileHandlerError(Exception):
@@ -131,9 +131,7 @@ def upload_dir(params: FilesLoadInfo):
     超过最大文件数量则退出;load_image为True时只支持图片类型, False时只支持文档类型;支持图片类型时,无需注册spliter;支持文档类型时必须注册spliter;
     embedding模型需要与支持的文件类型一致,否则会出现embedding错误.
     """
-    dir_path_obj = Path(dir_path)
-    if not dir_path_obj.is_dir():
-        raise FileHandlerError(f"dir path '{dir_path}' is invalid")
+    FileCheck.dir_check(dir_path)
     loader_types = []
     for file_types, _ in loader_mng.loaders.values():
         loader_types.extend(file_types)
