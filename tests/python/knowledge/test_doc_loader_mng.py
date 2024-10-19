a82e7e3d19a0a59ef12399fbe292347f8e0ebb00
developer <developer@com>
2024-10-19 14:23:41 +0800

    Match-id-0459765fd1bb7103f8ec1330f5467d9101ff25d9

--- tests/python/knowledge/test_doc_loader_mng.py
+++ tests/python/knowledge/test_doc_loader_mng.py
@@ -11,18 +11,19 @@ from mx_rag.knowledge.doc_loader_mng import LoaderMng, LoaderInfo, SplitterInfo
 
 class LoaderMngTestCase(unittest.TestCase):
     current_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.realpath(os.path.join(current_dir, "../../../data/test.xlsx"))
 
     def test_register_loader(self):
         loader_mng = LoaderMng()
         loader_mng.register_loader(ExcelLoader, [".xlsx"])
         loader_info = loader_mng.get_loader(".xlsx")
-        loader = loader_info.loader_class(file_path=os.path.join(self.current_dir, "../../../data/test.xlsx"))
+        loader = loader_info.loader_class(file_path=self.data_dir)
         self.assertIsInstance(loader, ExcelLoader)
 
     def test_register_splitter(self):
         loader_mng = LoaderMng()
         loader_mng.register_splitter(RecursiveCharacterTextSplitter, [".xlsx", ".docx", ".doc", ".pdf", ".pptx"],
-                                 {"chunk_size": 4000, "chunk_overlap": 20, "keep_separator": False})
+                                     {"chunk_size": 4000, "chunk_overlap": 20, "keep_separator": False})
         splitter_info = loader_mng.get_splitter(".xlsx")
         splitter = splitter_info.splitter_class(**splitter_info.splitter_params)
         self.assertIsInstance(splitter, RecursiveCharacterTextSplitter)
@@ -51,7 +52,6 @@ class LoaderMngTestCase(unittest.TestCase):
     def test_get_splitter(self):
         loader_mng = LoaderMng()
         loader_mng.register_splitter(RecursiveCharacterTextSplitter, [".xlsx", ".docx", ".doc", ".pdf", ".pptx"],
-                                 {"chunk_size": 4000, "chunk_overlap": 20, "keep_separator": False})
+                                     {"chunk_size": 4000, "chunk_overlap": 20, "keep_separator": False})
         splitter_info = loader_mng.get_splitter(".xlsx")
         self.assertIsInstance(splitter_info, SplitterInfo)
-
