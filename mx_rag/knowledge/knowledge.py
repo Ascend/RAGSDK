619c8db76ddba7f98d0a119038b0845493c1eb67
developer <developer@com>
2024-10-23 15:28:33 +0800

    Match-id-d876dadf1f1b4a4eb64dc484aabae1d641fc7082

--- mx_rag/knowledge/knowledge.py
+++ mx_rag/knowledge/knowledge.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from mx_rag.knowledge.base_knowledge import KnowledgeBase, KnowledgeError
 from mx_rag.storage.document_store.base_storage import Docstore, MxDocument
 from mx_rag.storage.vectorstore import VectorStore
-from mx_rag.utils.common import validate_params, INT_32_MAX, FILE_COUNT_MAX, \
+from mx_rag.utils.common import validate_params, INT_32_MAX, FILE_COUNT_MAX, validate_dict, \
     check_db_file_limit, validata_list_str, MAX_PATH_WHITE, TEXT_MAX_LEN, STR_TYPE_CHECK_TIP_1024
 from mx_rag.utils.file_check import FileCheck, check_disk_free_space
 
