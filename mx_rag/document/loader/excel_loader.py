5adc75d768c0da4060d519d10524432c30db1cfe
developer <developer@com>
2024-12-03 19:38:17 +0800

    Match-id-f7f34de32e36e705c174698d95aac18cd90b698f

--- mx_rag/document/loader/excel_loader.py
+++ mx_rag/document/loader/excel_loader.py
@@ -217,6 +217,7 @@ class ExcelLoader(BaseLoader):
         # 获取标题列表
         title = self._get_xlsx_title(ws, first_row, first_col)
 
+        max_line_width=800
         for row_index in range(first_row + 1, ws.max_row + 1):
             # 空行无数据，不解析
             if row_index in blank_rows.keys():
@@ -231,6 +232,12 @@ class ExcelLoader(BaseLoader):
                 val = self._parse_xlsx_cell(ws, row_index, col_index)
                 ti = title[col_index - first_col]
                 text_line += str(ti) + ":" + str(val) + ";"
+
+            # 计算行的字符数
+            line_width = len(text_line)
+            # 检查行宽是否超过最大行宽
+            if line_width > max_line_width:
+                raise ValueError(f"Line width exceeds maximum allowed width of {max_line_width} characters.")
 
             content += text_line + self.line_sep
 
