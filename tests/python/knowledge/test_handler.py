68d921a579b1303b8e228def1fa7cd7190076cea
developer <developer@com>
2024-12-28 16:13:50 +0800

    Match-id-a1a2126dbfbcbb3d2036beeab25d9c79c52aeaea

--- tests/python/knowledge/test_handler.py
+++ tests/python/knowledge/test_handler.py
@@ -31,6 +31,13 @@ class TestHandler(unittest.TestCase):
     white_paths = os.path.realpath(os.path.join(current_dir, "../../data/"))
     test_file = os.path.realpath(os.path.join(current_dir, "../../data/test.pdf"))
     test_folder = os.path.realpath(os.path.join(current_dir, "../../data/files/"))
+    loader_mng = LoaderMng()
+    loader_mng.register_loader(DocxLoader, [".docx"])
+    loader_mng.register_loader(PdfLoader, [".pdf"])
+
+    loader_mng.register_splitter(RecursiveCharacterTextSplitter,
+                                 [".docx", ".pdf"],
+                                 {"chunk_size": 4000, "chunk_overlap": 20, "keep_separator": False})
 
     def setUp(self):
         # 先清空临时数据库
@@ -38,13 +45,6 @@ class TestHandler(unittest.TestCase):
             os.remove(SQL_PATH)
 
     def create_knowledge_db(self, knowledge_name="test001"):
-        loader_mng = LoaderMng()
-        loader_mng.register_loader(DocxLoader, [".docx"])
-        loader_mng.register_loader(PdfLoader, [".pdf"])
-
-        loader_mng.register_splitter(RecursiveCharacterTextSplitter,
-                                     [".docx", ".pdf"],
-                                     {"chunk_size": 4000, "chunk_overlap": 20, "keep_separator": False})
         vector_store = MagicMock(spec=MindFAISS)
         vector_store.add = MagicMock(return_value=None)
         chunk_store = SQLiteDocstore(db_path=SQL_PATH)
@@ -54,20 +54,20 @@ class TestHandler(unittest.TestCase):
 
     def test_upload_files_with_invalid_knowledge(self):
         with self.assertRaises(ValueError):
-            upload_files(knowledge=None, files=[self.test_file], loader_mng=LoaderMng(),
+            upload_files(knowledge=None, files=[self.test_file], loader_mng=self.loader_mng,
                          embed_func=embed_func, force=True)
 
     def test_upload_files_with_invalid_file_paths(self):
         knowledge_db = self.create_knowledge_db()
         with self.assertRaises(FileCheckError):
-            upload_files(knowledge=knowledge_db, files=['/test/test.docx' * 100], loader_mng=LoaderMng(),
+            upload_files(knowledge=knowledge_db, files=['/test/test.docx' * 100], loader_mng=self.loader_mng,
                          embed_func=embed_func, force=True)
 
     def test_upload_files_with_too_many_files(self):
         knowledge_db = self.create_knowledge_db()
         knowledge_db.max_file_count = 1
         with self.assertRaises(FileHandlerError):
-            upload_files(knowledge=knowledge_db, files=[self.test_file, self.test_file], loader_mng=LoaderMng(),
+            upload_files(knowledge=knowledge_db, files=[self.test_file, self.test_file], loader_mng=self.loader_mng,
                          embed_func=embed_func, force=True)
 
     def test_upload_files_with_invalid_loader(self):
@@ -79,26 +79,26 @@ class TestHandler(unittest.TestCase):
     def test_upload_files_with_invalid_embed_func(self):
         knowledge_db = self.create_knowledge_db()
         with self.assertRaises(ValueError):
-            upload_files(knowledge=knowledge_db, files=[self.test_file], loader_mng=LoaderMng(),
+            upload_files(knowledge=knowledge_db, files=[self.test_file], loader_mng=self.loader_mng,
                          embed_func=None, force=True)
 
     def test_upload_files_with_add_file_failure(self):
         knowledge_db = self.create_knowledge_db()
         with patch('mx_rag.knowledge.KnowledgeDB.add_file') as mock_add_file:
             mock_add_file.side_effect = Exception('Add file failed')
-            result = upload_files(knowledge=knowledge_db, files=[self.test_file], loader_mng=LoaderMng(),
+            result = upload_files(knowledge=knowledge_db, files=[self.test_file], loader_mng=self.loader_mng,
                                   embed_func=embed_func, force=True)
             self.assertEqual(result, self.test_file)
 
     def test_upload_files_success(self):
         knowledge_db = self.create_knowledge_db()
-        result = upload_files(knowledge=knowledge_db, files=[self.test_file], loader_mng=LoaderMng(),
+        result = upload_files(knowledge=knowledge_db, files=[self.test_file], loader_mng=self.loader_mng,
                               embed_func=embed_func, force=True)
         self.assertEqual(result, [])
 
     def test_upload_dir(self):
         knowledge_db = self.create_knowledge_db()
-        params = FilesLoadInfo(knowledge=knowledge_db, dir_path=self.test_folder, loader_mng=LoaderMng(),
+        params = FilesLoadInfo(knowledge=knowledge_db, dir_path=self.test_folder, loader_mng=self.loader_mng,
                                embed_func=embed_func, force=True, load_image=False)
         upload_dir(params=params)
 
