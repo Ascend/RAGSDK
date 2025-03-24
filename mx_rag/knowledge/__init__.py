8e1d598eca1edac65f5f6df25e1375205a927a91
developer <developer@com>
2025-03-24 09:28:48 +0800

    Match-id-8359f3a0dc78a0ab4116ff7739b3e4bb60d6475e

--- mx_rag/knowledge/__init__.py
+++ mx_rag/knowledge/__init__.py
@@ -10,5 +10,4 @@ __all__ = [
 ]
 
 from mx_rag.knowledge.handler import upload_dir, upload_files, delete_files, FilesLoadInfo
-from mx_rag.knowledge.knowledge import KnowledgeDB, KnowledgeStore, KnowledgeMgrStore, KnowledgeModel, DocumentModel
-from mx_rag.knowledge.knowledge import KnowledgeDB, KnowledgeStore
+from mx_rag.knowledge.knowledge import KnowledgeDB, KnowledgeStore, KnowledgeModel, DocumentModel
