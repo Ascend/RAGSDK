2ade55d928d81b6e934ea62e0950ed4c2d05c973
developer <developer@com>
2025-06-18 11:15:36 +0800

    Match-id-bf8d5cf4f512bfabcb1d2f6eaaf9f610ab0ee7ee

--- mx_rag/gvstore/graph_creator/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-__all__ = [
-    "KGEngine"
-]
-
-from mx_rag.gvstore.graph_creator.kg_engine import KGEngine
