b7df472f94d41121bbfa9fa427fa4a2aa93c6923
developer <developer@com>
2025-12-18 11:19:41 +0800

    Match-id-974fed6ccf56e22a5b3657e795c2d737a065bdce

--- mx_rag/retrievers/bm_retriever.py
+++ mx_rag/retrievers/bm_retriever.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------------------------------
-This file is part of the MindStudio project.
+This file is part of the RAGSDK project.
 Copyright (c) 2025 Huawei Technologies Co.,Ltd.
 
-MindStudio is licensed under Mulan PSL v2.
+RAGSDK is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
 
@@ -38,7 +38,7 @@ _KEY_WORD_TEMPLATE_ZH = PromptTemplate(
 Question: CANN如何安装？
 Keywords: CANN, 安装, install
 
-Question: MindStudio 容器镜像怎么制作
+Question: RAGSDK 容器镜像怎么制作
 Keywords: MindStudio, 容器镜像, Docker build
 
 Question: {question}
--- tests/python/document_store/test_helper_docstore.py
+++ tests/python/document_store/test_helper_docstore.py
@@ -2,10 +2,10 @@
 # encoding: utf-8
 """
 -------------------------------------------------------------------------
-This file is part of the MindStudio project.
+This file is part of the RAGSDK project.
 Copyright (c) 2025 Huawei Technologies Co.,Ltd.
 
-MindStudio is licensed under Mulan PSL v2.
+RAGSDK is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
 
