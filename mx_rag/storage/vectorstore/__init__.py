444b4108795599c209e7147509a09b688be7bd1e
public ostmssso <p_ostmssso_nobody@codehub.huawei.com>
2025-12-23 17:33:26 +0800

    RAGSDK仓库初始化

---
+++ mx_rag/storage/vectorstore/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------------------------------
+This file is part of the RAGSDK project.
+Copyright (c) 2025 Huawei Technologies Co.,Ltd.
+
+RAGSDK is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+
+         http://license.coscl.org.cn/MulanPSL2
+
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+-------------------------------------------------------------------------
+"""
+
+
+__all__ = [
+    "MindFAISS",
+    "VectorStore",
+    "MilvusDB",
+    "OpenGaussDB",
+    "VectorStorageFactory",
+    "SearchMode"
+]
+
+from .faiss_npu import MindFAISS
+from .vectorstore import VectorStore, SearchMode
+from .milvus import MilvusDB
+from .opengauss import OpenGaussDB
+from .vector_storage_factory import VectorStorageFactory
