ac30b6b1fb08bf66dc2c4c5273f12565babc5a23
printSSS <wangting287@h-partners.com>
2025-12-27 15:27:54 +0800

    update: 更新文件 setup.py
    Signed-off-by: printSSS <wangting287@h-partners.com>

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def get_ci_version_info():
     """
     src_path = this_directory.parent
     ci_version_file = src_path.joinpath('build', 'conf', 'config.yaml')
-    version = '6.0.RC2'
+    version = '7.0.RC1'
     logging.info("get version from %s", ci_version_file)
     try:
         R_FLAGS = os.O_RDONLY
