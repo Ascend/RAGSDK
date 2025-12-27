1692048944deb7a08ac0e3c6f3f605ca29664c4d
printSSS <wangting287@h-partners.com>
2025-12-27 19:22:56 +0800

    update: 更新文件 setup.py
    Signed-off-by: printSSS <wangting287@h-partners.com>

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def get_ci_version_info():
     :return: version number
     """
     src_path = this_directory.parent
-    ci_version_file = src_path.joinpath('build', 'conf', 'config.yaml')
+    ci_version_file = src_path.joinpath('build', 'conf', 'config.ini')
     version = '7.0.RC1'
     logging.info("get version from %s", ci_version_file)
     try:
